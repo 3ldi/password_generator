202e957a1e4687be04f9f033fcf349ab00f9e5ce
eldi <66099058+3ldi@users.noreply.github.com>
2022-12-24 02:26:01 +0000

    fixed random loop from for to while

--- passgenerator/views.py
+++ passgenerator/views.py
@@ -4,16 +4,20 @@ from django.shortcuts import render
 
 # Create homepage view
 def home(request):
+    """Render the homepage"""
     return render(request, 'passgenerator/home.html')
 
 
 # Create about view
 def about(request):
+    """Render the about page"""
     return render(request, 'passgenerator/about.html')
 
 
 # Create the password generator output page
 def password(request):
+    """Based on user selection the function will extend the lowercase list 
+        to include the list selected by the user"""
 
     characters = list('abcdefghijklmnopqrstuvwxyz')
     uppercase = list('QWERTYUIOPLKJHGFDSAZXCVBNM')
@@ -32,12 +36,18 @@ def password(request):
     if request.GET.get('numbers'):
         characters.extend(numbers)
 
+    # The default length will be set to 12
     length = int(request.GET.get('length', 12))
 
     the_password = ''
 
-    for char in range(length):
+    i = 0
+    while i < length:
         the_password += random.choice(characters)
+        i += 1
+
+    # for char in range(length):
+    #     the_password += random.choice(characters)
 
     return render(request, 'passgenerator/password.html', {
         'password': the_password
