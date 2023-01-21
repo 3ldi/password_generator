86e3f98c687ffa8f97174d3ab39111b7d0c76ead
eldi <66099058+3ldi@users.noreply.github.com>
2023-01-20 19:59:34 -0500

    set debug mode to false

--- password_generator/settings.py
+++ password_generator/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dl9!0iopp9i*z1q(0ox^5!+*#5*^ir^!@(g+v1_)l_yef#za@@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
